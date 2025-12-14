9ca21e011124d1be56b130b6b1f503f7b345c139
Srinidhi <srinithidr@gmail.com>
2025-12-14 13:17:47 +0530

    Initial commit for new repo

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/config/inngest";
+import { createClient } from 'inngest';
 
 // VERY IMPORTANT FOR VERCEL + NEXT 16
 export const runtime = "nodejs";
